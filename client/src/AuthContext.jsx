a601064d308187c461b111297338bd610f5b8c0e
housewithwindows <tsikoridzenino1963@gmail.com>
2026-01-22 20:59:56 +0400

    seventh commit

--- client/src/AuthContext.jsx
+++ client/src/AuthContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useContext, useState } from "react";
 export const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL + '/api';
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
--- server/app.js
+++ server/app.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express'); 
+const dotenv = require('dotenv'); 
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
+const path = require('path');
+
+
 
 // Routers
 const tourRouter = require('./router/tour.router.js');
 const authRouter = require('./router/auth.router.js');
 
-
 const globalErrorHandler = require('./utils/globalErrorHandler.js');
-
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
-}
+};
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://tourify-1.onrender.com/",
     credentials: true               
 }));
-
-
 
 app.use(cookieParser());
 app.use(express.json());
 
-
 app.use('/api/auth', authRouter);
 app.use('/api/tours', tourRouter);
-
-
-
 
 app.use(globalErrorHandler);
 
