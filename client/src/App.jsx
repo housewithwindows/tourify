beeac8a1a28428ad3c8dbbc6a3f78a0427ed8394
housewithwindows <tsikoridzenino1963@gmail.com>
2026-01-25 18:54:23 +0400

    thirteenth commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Nav from "./Nav";
 import Home from "./Home";
 import LogIn from "./LogIn";
@@ -7,8 +8,22 @@ import Profile from "./Profile";
 import Tours from "./Tours";
 import LogoutButton from "./LogOut";
 
-
 const App = () => {
+  useEffect(() => {
+    if (window.Tawk_API) return; // prevent duplicate load
+
+    var Tawk_API = window.Tawk_API || {};
+    var Tawk_LoadStart = new Date();
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/6973ae160423e41981a1411d/1jflu02qk";
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
       <Nav />
@@ -18,11 +33,12 @@ const App = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tours" element={<Tours />} />
-        <Route path="/logout" element={<LogoutButton/>}/>
+        <Route path="/logout" element={<LogoutButton />} />
       </Routes>
     </>
   );
 };
 
 export default App;
+
 
